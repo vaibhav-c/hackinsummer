246a4342e68f9eeec5851bc8bce1302c6b96a576
Atishay <xatishayx@pop-os.localdomain>
2021-01-28 05:47:23 +0530

    time_fix

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 14, 2021 23:59:59").getTime();
+var countDownDate = new Date("Feb 13, 2021 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
