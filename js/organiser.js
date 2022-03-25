70ef4d1946bf0117c8ffe12ef3bafbb71009f40b
vaibhav-c <vaibhav.c987@gmail.com>
2022-03-25 20:22:15 +0530

    minor errors resolved

--- js/organiser.js
+++ js/organiser.js
@@ -61,35 +61,3 @@ jQuery("#carousel-1").owlCarousel({
       }
     }
   });
-
-  jQuery("#carousel-3").owlCarousel({
-    autoplay: false,
-    rewind: false, /* use rewind if you don't want loop */
-    margin: 20,
-     /*
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    */
-    responsiveClass: true,
-    autoHeight: true,
-    autoplayTimeout: 7000,
-    smartSpeed: 100,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-  
-      600: {
-        items: 3
-      },
-  
-      1024: {
-        items: 4
-      },
-  
-      1366: {
-        items: 4
-      }
-    }
-  });
