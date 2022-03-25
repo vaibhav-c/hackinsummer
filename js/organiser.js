53e59801d89bc43bf532a86b58d001686285664d
vaibhav-c <vaibhav.c987@gmail.com>
2022-03-25 19:46:57 +0530

    added faculty

--- js/organiser.js
+++ js/organiser.js
@@ -1,4 +1,68 @@
-jQuery("#carousel").owlCarousel({
+jQuery("#carousel-1").owlCarousel({
+    autoplay: false,
+    rewind: false, /* use rewind if you don't want loop */
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 100,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 4
+      },
+  
+      1366: {
+        items: 4
+      }
+    }
+  });
+
+  jQuery("#carousel-2").owlCarousel({
+    autoplay: false,
+    rewind: false, /* use rewind if you don't want loop */
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 100,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 4
+      },
+  
+      1366: {
+        items: 4
+      }
+    }
+  });
+
+  jQuery("#carousel-3").owlCarousel({
     autoplay: false,
     rewind: false, /* use rewind if you don't want loop */
     margin: 20,
