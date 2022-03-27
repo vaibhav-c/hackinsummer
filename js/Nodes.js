b57c540d5a4f7602e1c030f77e7afddd151e12ce
vaibhav-c <vaibhav.c987@gmail.com>
2022-03-27 23:36:45 +0530

    svgs made

--- js/Nodes.js
+++ js/Nodes.js
@@ -50,7 +50,7 @@ var Nodes = {
     }
 
     // Load initial input image (the chrome logo!)
-    this.loadData('../assets/island_scene.svg');
+    this.loadData('../assets/about.svg');
   },
 
   preparePoints: function() {
