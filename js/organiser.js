1912971a26c6adb24c74037c25f0d774bec10f1c
vaibhav-c <vaibhav.c987@gmail.com>
2022-04-02 16:43:42 +0530

    final code

--- js/organiser.js
+++ js/organiser.js
@@ -21,6 +21,38 @@ jQuery("#carousel-1").owlCarousel({
       },
   
       1024: {
+        items: 3
+      },
+  
+      1366: {
+        items: 3
+      }
+    }
+  });
+
+  jQuery("#carousel-3").owlCarousel({
+    autoplay: false,
+    rewind: false, /* use rewind if you don't want loop */
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 100,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
         items: 4
       },
   
